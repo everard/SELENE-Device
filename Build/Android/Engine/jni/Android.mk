bc791edb95e4d59e2280876f6fce105055399b85
Nezametdinov Ildus <neil.log@gmail.com>
2012-11-16 21:05:23 +0400

    update android build system

--- Build/Android/Engine/jni/Android.mk
+++ Build/Android/Engine/jni/Android.mk
@@ -1,8 +1,9 @@
-LOCAL_PATH := $(call my-dir)/../../../../Source/Engine
+LOCAL_PATH := $(call my-dir)/../../../../Source
 include $(CLEAR_VARS)
 
-SEARCH_DIRS = /* /*/* /*/*/* /*/*/*/* /*/*/*/*/* /*/*/*/*/*/*
+MY_SEARCH_DIRS = /* /*/* /*/*/* /*/*/*/* /*/*/*/*/* /*/*/*/*/*/*
+MY_SOURCE_DIRS = /Engine /Platforms/Android
 LOCAL_MODULE    := engine
-LOCAL_SRC_FILES := $(patsubst $(LOCAL_PATH)/%, %, $(foreach DIR, $(SEARCH_DIRS), $(wildcard $(LOCAL_PATH)$(DIR).cpp)))
+LOCAL_SRC_FILES := $(foreach SRC_DIR, $(MY_SOURCE_DIRS), $(patsubst $(LOCAL_PATH)/%, %, $(foreach DIR, $(MY_SEARCH_DIRS), $(wildcard $(LOCAL_PATH)$(SRC_DIR)$(DIR).cpp))))
 
 include $(BUILD_STATIC_LIBRARY)
